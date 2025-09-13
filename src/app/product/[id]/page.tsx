548e8feefbe33244836ea69a21be7b836df8661a
Agbi Dave <agbidave40@gmail.com>
2025-09-13 07:54:50 +0000

    i'm getting this error Error: Command "npm run build" exited with 1

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -2,7 +2,6 @@
 import { getProduct, getProducts } from "@/lib/firestore";
 import ProductDetails from "./product-details";
 import { Header } from "@/components/header";
-import { Skeleton } from "@/components/ui/skeleton";
 import { notFound } from 'next/navigation';
 
 export async function generateStaticParams() {
@@ -10,31 +9,6 @@ export async function generateStaticParams() {
   return products.map((product) => ({
     id: product.id,
   }));
-}
-
-function ProductPageFallback() {
-    return (
-        <>
-            <Header />
-            <main className="container mx-auto px-4 py-8">
-                <div className="grid md:grid-cols-2 gap-12">
-                    <div>
-                        <Skeleton className="aspect-square rounded-lg" />
-                    </div>
-                    <div className="space-y-4">
-                        <Skeleton className="h-10 w-3/4" />
-                        <Skeleton className="h-6 w-1/2" />
-                        <Skeleton className="h-8 w-1/4" />
-                        <Skeleton className="h-20 w-full" />
-                        <div className="flex gap-4">
-                            <Skeleton className="h-12 w-32" />
-                            <Skeleton className="h-12 w-32" />
-                        </div>
-                    </div>
-                </div>
-            </main>
-        </>
-    )
 }
 
 export default async function ProductPage({ params }: { params: { id: string } }) {
